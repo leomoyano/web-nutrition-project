1c2fe9ab128d0f950616835d805e995c07aa2c94
unknown <leomoyano7@gmail.com>
2020-05-17 13:25:23 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+const APP_ID = 'cfeeb493';
+const APP_KEY = '59c36bf8176ad6043253f8aec99fa50d';
+
+useEffect(() =>{
+  getRecipes();
+},[])
+
+const getRecipes = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  console.log(data.hits);
+  
+}
+
+  return(
     <div className="App">
-      <h1>Nuevo proyecto</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
-
 export default App;
